ccb2bdd082eed4b51ca72d3586a0c12d61587d2d
UncleSocks <ilisan.tyrone@gmail.com>
2026-03-01 12:07:01 +0800

    Minor Update: Added LICENSE in the source code.

--- fbintel.py
+++ fbintel.py
@@ -1,3 +1,25 @@
+# FBIntelPy: Python Facebook Intelligence Search Tool
+# GitHub @unclesocks: https://github.com/UncleSocks
+#
+# MIT License
+#
+# Copyright (c) 2026 Tyrone Kevin Ilisan
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT 
+# NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+
+
 import os
 import sys
 import base64
@@ -372,7 +394,6 @@ class WidgetLogicController:
 
 
 class GenerateWidgets:
-
     def __init__(self, root):
         self.root = root
         self._root_setup()
@@ -455,6 +476,6 @@ class GenerateWidgets:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    widgets = GenerateWidgets(root)
-    widgets.display_widgets()
+    init = GenerateWidgets(root)
+    init.display_widgets()
     root.mainloop()
